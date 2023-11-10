b5e8c1fac6d47738242b8b47c03df6aac70804d2
mattc <tingyugenius@gmail.com>
2023-11-10 13:36:16 +0800

    Initial Commit

---
+++ MetalShader-101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalShader-101
+//
+//  Created by matt on 2023/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalShader-101/MetalShader_101App.swift
@@ -0,0 +1,17 @@
+//
+//  MetalShader_101App.swift
+//  MetalShader-101
+//
+//  Created by matt on 2023/11/10.
+//
+
+import SwiftUI
+
+@main
+struct MetalShader_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
